a419013fa8ad34e0cb1f948740b5b30880e7f43e
Maikel Karam <maikel.m.karam@gmail.com>
2022-12-14 14:49:30 -0600

    Bonus 1 completed

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -12,15 +12,24 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import javax.swing.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+        response.setContentType ("text/html");
+        String name = request.getParameter("name");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        String cust = name == (null) || name.isEmpty()?"World":name;
+        out.printf("<h1>Hello %s</h1>", cust);
+        out.println("Hello, world");
+        out.println("Hey there");
+
+//        String query = request.getParameter("some");
+//        PrintWriter out = response.getWriter();
+//        out.println("<h1>Hello, World!</h1>");
     }
 
 }
